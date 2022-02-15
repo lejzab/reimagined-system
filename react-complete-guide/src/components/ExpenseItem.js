ed3b654b747faf99f64b31f6bce7de987874d545
Paweł <pawel.bogaczewicz@outlook.com>
2022-02-15 11:49:35 +0100

    data not hardcoded

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
-import './ExpenseItem.css';
+import './ExpenseItem.css'
 
 function ExpenseItem() {
+    const expenseDate = new Date(2021, 2, 28);
+    const expenseTitle = 'Car insurance';
+    const expenseAmount = 294.67;
+
     return (
         <div className="expense-item">
-            <div>Feb 14 2022</div>
+            <div>{expenseDate.toLocaleDateString()}</div>
             <div className="expense-item__description">
-                <h2>Car insurance</h2>
-                <div className="expense-item__price">$643.45</div>
+                <h2>{expenseTitle}</h2>
+                <div className="expense-item__price">${expenseAmount}</div>
             </div>
         </div>
     );
