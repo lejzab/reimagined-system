75afbc45f9e610f81715eb900c79a904e143b6db
Paweł <0x645@tutanota.com>
2022-02-17 19:17:34 +0100

    conditional 1

--- react-complete-guide/src/components/Expenses/Expenses.js
+++ react-complete-guide/src/components/Expenses/Expenses.js
@@ -17,15 +17,22 @@ const Expenses = (props) => {
 
     return (
         <Card className="expenses">
-            <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangedHandler}/>
-            {filteredExpenses.map((expense) => (
-                <ExpenseItem
-                    key={expense.id}
-                    title={expense.title}
-                    amount={expense.amount}
-                    date={expense.date}
-                />
-            ))}
+            <ExpensesFilter selected={filteredYear}
+                            onChangeFilter={filterChangedHandler}
+            />
+            {filteredExpenses.length === 0 ? (
+                <p>No expenses found!</p>
+            ) : (
+                filteredExpenses.map((expense) => (
+                        <ExpenseItem
+                            key={expense.id}
+                            title={expense.title}
+                            amount={expense.amount}
+                            date={expense.date}
+                        />
+                    )
+                )
+            )}
         </Card>
     );
 }
