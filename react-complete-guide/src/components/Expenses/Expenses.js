d572c251d9f8ca83f5ec4bf1120781cd92497ea8
Paweł <0x645@tutanota.com>
2022-02-17 19:44:36 +0100

    conditional 4

--- react-complete-guide/src/components/Expenses/Expenses.js
+++ react-complete-guide/src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
-import ExpenseItem from "./ExpenseItem";
 import './Expenses.css'
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import {useState} from "react";
+import ExpensesList from "./ExpensesList";
 
 const Expenses = (props) => {
     const [filteredYear, setFilteredYear] = useState('2019');
@@ -15,24 +15,13 @@ const Expenses = (props) => {
         return expense.date.getFullYear().toString() === filteredYear;
     });
 
-    let expensesContent = <p>No expenses found!</p>;
-    if (filteredExpenses.length > 0) {
-        expensesContent = filteredExpenses.map((expense) => (
-                <ExpenseItem
-                    key={expense.id}
-                    title={expense.title}
-                    amount={expense.amount}
-                    date={expense.date}
-                />
-            ));
-    }
 
     return (
         <Card className="expenses">
             <ExpensesFilter selected={filteredYear}
                             onChangeFilter={filterChangedHandler}
             />
-            {expensesContent}
+            <ExpensesList items={filteredExpenses}/>
         </Card>
     );
 }
