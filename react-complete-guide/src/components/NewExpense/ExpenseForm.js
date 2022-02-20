eb506fa63f76829ea7b53f3d274c1f5a5d0f15cd
Paweł <0x645@tutanota.com>
2022-02-20 21:30:27 +0100

    fixed bug in adding new expense - convert amount to number

--- react-complete-guide/src/components/NewExpense/ExpenseForm.js
+++ react-complete-guide/src/components/NewExpense/ExpenseForm.js
@@ -20,7 +20,7 @@ const ExpenseForm = (props) => {
         event.preventDefault();
         const expenseData = {
             title: enteredTitle,
-            amount: +enteredAmount,
+            amount: parseFloat(enteredAmount),
             date: new Date(enteredDate)
         };
         props.onSaveExpenseData(expenseData);
