6d2a746e75835f5611fa978ffdb68cd51a2565f5
Paweł <0x645@tutanota.com>
2022-02-20 13:41:30 +0100

    graph by mont. not working ok right now though

--- react-complete-guide/src/components/Expenses/Expenses.js
+++ react-complete-guide/src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import {useState} from "react";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 const Expenses = (props) => {
     const [filteredYear, setFilteredYear] = useState('2019');
@@ -21,6 +22,7 @@ const Expenses = (props) => {
             <ExpensesFilter selected={filteredYear}
                             onChangeFilter={filterChangedHandler}
             />
+            <ExpensesChart expenses={filteredExpenses}/>
             <ExpensesList items={filteredExpenses}/>
         </Card>
     );
